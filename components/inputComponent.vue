851176168249de1080224453fc110cfc6883b5b4
juhee <qkr30510@naver.com>
2021-09-02 23:48:00 +0900

    초기셋팅

---
+++ components/inputComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+         <!-- v-if="!this.remNumberWrap" -->
+        <div>
+            <input v-model="bodyNumber" type="number" placeholder="최상단 입력값을 넣어주세요.">
+            <button @click="change" class="uk-button uk-button-primary">변환하기</button>
+        </div>
+        <div >
+            <ul>
+                <li v-for="(number, i) in numberList" v-bind:key='i' >
+                    <span>{{i}}</span>
+                    <span>{{number}}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'inputComponent',
+    data() {
+        return {
+            bodyNumber:'',
+            pxNumber:'',
+            remNumberWrap:false,
+            numberList:[],
+
+        };
+    },
+    methods: {
+        change(){
+            this.remNumberWrap = true;
+            let newNumbers = 0
+            
+            
+            for(let i = 8; i < 30; i++){
+                newNumbers = i/this.bodyNumber
+                this.numberList.push(newNumbers)
+                console.log(this.numberValue)
+            }
+            
+        }
+    },
+    computed: {
+
+    },
+    mounted() {
+        
+    },
+    // 변환하기 누르면 입력된 숫자가 1rem 기준으로 바꿈.
+    // 사용하려는 값 / 최상단 부모값 = rem 값
+};
+</script>
+
+
+
+
+
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h1>px을 => rem 바꾸기</h1>
+    <div>
+      <inputComponent/>
+    </div>
+  </div>
+</template>
+
+<script>
+import input from '../components/inputComponent.vue'
+export default {
+  components: input,
+  
+  }
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;  
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
